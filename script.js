933d5c030b6c0c904a2224bf4282d182967f0204
caio <159967666+caio1274@users.noreply.github.com>
2026-01-08 11:50:08 -0300

    site inicial

---
+++ script.js
@@ -0,0 +1,14 @@
+/* 
+Função que recebe o retângulo como parâmetro.
+Através do "this", identifica qual retângulo está em interação.
+*/
+
+function entrou(retangulo) {
+    // Verde claro mais suave e agradável para leitura
+    retangulo.style.background = '#a5d6a7';
+}
+
+function sair(retangulo) {
+    // Verde mais escuro e confortável aos olhos
+    retangulo.style.background = '#2e7d32';
+}
